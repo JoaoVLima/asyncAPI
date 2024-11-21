f10443baf7bcb501a9072bc4a14aea42d243aa83
JoaoVLima <celu.joaolima@gmail.com>
2024-11-21 17:25:51 -0300

    main in app folder

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ app = FastAPI(
                      para armazenamento temporário de resultados, \
                      com suporte a implantação via Docker.",
         version="0.0.1",
-        default_response_class=ORJSONResponse,
         contact={
             "name": "João Lima",
             "url": "https://limadeveloper.com/",
             "email": "joao@limadeveloper.com",
         },
+        default_response_class=ORJSONResponse,
         debug=True,
 )
 
@@ -24,6 +24,21 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
+
+
+@app.post("/scrape")
+async def scrape(cnpj: str):
+    task_id = await send_task_to_queue(cnpj)
+    return {"task_id": task_id}
+
+
+@app.get("/results/{task_id}")
+async def get_results(task_id: str):
+    status = await get_task_status(task_id)
+    if status == "completed":
+        result = await get_task_result(task_id)
+        return {"status": status, "result": result}
+    return {"status": status}
 
 
 @app.get("/")
